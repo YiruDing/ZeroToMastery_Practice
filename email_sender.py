9d31f114b9506de78a4cd518bd1096143e4490c1
Yi-Ru Ding <dingyr0925@gmail.com>
2022-07-18 23:38:17 -0400

    July 18

--- email_sender.py
+++ email_sender.py
@@ -1,3 +1,6 @@
+# # 220-223
+# # 22 07/17 To be fixed...
+# 534, b'5.7.9 Application-specific password required. Learn more at\n5.7.9  https://support.google.com/mail/?p=InvalidSecondFactor i12-20020a37c20c000000b006a6a6f148e6sm11159446qkm.17 - gsmtp')
 import smtplib
 # Allow us to build a SMTP server
 from email.message import EmailMessage
@@ -14,6 +17,7 @@ with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     # Check the protocol to learn more
     smtp.starttls()
-    smtp.login('dingyr0925@gmail.com', 'Bddongding1!')
+    smtp.login('dingyr0925@gmail.com', 'yghfzmpchhsrfbbd')
+    # ahhh，是授權的十六碼啦！
     smtp.send_message(email)
     print('All good boss!')
